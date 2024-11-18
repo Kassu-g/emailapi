b41e74d20619bfcd1ab07b5ee57b9021d149cb16
kasper666g <64731263+kasper666g@users.noreply.github.com>
2024-11-18 22:19:42 +0200

    ddd

--- public/main.js
+++ public/main.js
@@ -1,15 +1,47 @@
-document.getElementById('getUsers').addEventListener('click', function() {
-  fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(users => {
-      const userList = document.getElementById('userList');
-      userList.innerHTML = '';
-      
-      users.forEach(user => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${user.name} - ${user.email}`;
-        userList.appendChild(listItem);
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  const userForm = document.getElementById('userForm');
+  
+  userForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
     })
-    .catch(error => console.error('Error fetching users:', error));
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      alert('User successfully added');
+      userForm.reset();
+    })
+    .catch(error => console.error('Error:', error));
+  });
+
+  const getUsersButton = document.getElementById('getUsers');
+  
+  getUsersButton.addEventListener('click', () => {
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(users => {
+        const userList = document.getElementById('userList');
+        userList.innerHTML = '';
+
+        users.forEach(user => {
+          const listItem = document.createElement('li');
+          listItem.textContent = `${user.name} - ${user.email}`;
+          userList.appendChild(listItem);
+        });
+      })
+      .catch(error => console.error('Error fetching users:', error));
+  });
 });
+
